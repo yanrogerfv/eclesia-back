bebd954e6d299bf400de726bf709f819f37ca381
yan-roger-fv <yanrogerfv@gmail.com>
2024-06-13 15:05:51 -0300

    feat: creation of ImportantDate class

---
+++ src/main/java/imdl/scalator/domain/ImportantDate.java
@@ -0,0 +1,11 @@
+package imdl.scalator.domain;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class ImportantDate {
+    private UUID id;
+    private String titulo;
+    private LocalDate data;
+    private Escala escala;
+}

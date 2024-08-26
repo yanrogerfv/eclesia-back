33aed9ed8557b3292eae128e058ed8de87edd124
yan-roger-fv <yanrogerfv@gmail.com>
2024-08-26 15:30:08 -0300

    refac: changed Levita to work purely with Java

--- src/main/java/imdl/scalator/domain/Levita.java
+++ src/main/java/imdl/scalator/domain/Levita.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import java.util.List;
 import java.util.UUID;
 
-@Data
 public class Levita {
     private UUID id;
     private String nome;
@@ -13,4 +12,52 @@ public class Levita {
     private String contato;
     private String email;
     private boolean disponivel;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Instrumento> getInstrumentos() {
+        return instrumentos;
+    }
+
+    public void setInstrumentos(List<Instrumento> instrumentos) {
+        this.instrumentos = instrumentos;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
 }

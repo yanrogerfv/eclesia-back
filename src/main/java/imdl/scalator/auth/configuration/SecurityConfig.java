6ba3663a2195c42b042c44b6a3f3301513e84940
yan-roger-fv <yanrogerfv@gmail.com>
2024-10-02 14:25:57 -0300

    feat: development of security module

--- src/main/java/imdl/scalator/auth/configuration/SecurityConfig.java
+++ src/main/java/imdl/scalator/auth/configuration/SecurityConfig.java
@@ -1,24 +1,42 @@
 package imdl.scalator.auth.configuration;
 
 import org.aspectj.weaver.patterns.IToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("admin")
+        UserDetails user = User.withDefaultPasswordEncoder()
+                            .username("admin")
                             .password("password")
                             .roles("ADMIN")
                             .build();
@@ -26,9 +44,12 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll());
-        http.csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+//        http.authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/**").permitAll());
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults()).build();
     }
 }
--- src/main/java/imdl/scalator/auth/repository/UserRepository.java
+++ src/main/java/imdl/scalator/auth/repository/UserRepository.java
@@ -3,7 +3,11 @@ package imdl.scalator.auth.repository;
 import imdl.scalator.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByUsername(String username);
 }

f7e486aca9481914dca988801f5cccb3037e8aa2
yan-roger-fv <yanrogerfv@gmail.com>
2024-11-27 15:38:43 -0300

    feat: added field descricao to Levita

--- src/main/java/imdl/scalator/service/mapper/LevitaMapper.java
+++ src/main/java/imdl/scalator/service/mapper/LevitaMapper.java
@@ -12,9 +12,10 @@ public class LevitaMapper {
         Levita domain = new Levita();
         domain.setId(entity.getId());
         domain.setNome(entity.getNome());
-        domain.setInstrumentos(entity.getInstrumentos().stream().map(InstrumentoMapper::entityToDomain).sorted(Comparator.comparingLong(Instrumento::getId)).toList());
-        domain.setContato(entity.getContato());
         domain.setEmail(entity.getEmail());
+        domain.setContato(entity.getContato());
+        domain.setDescricao(entity.getDescricao());
+        domain.setInstrumentos(entity.getInstrumentos().stream().map(InstrumentoMapper::entityToDomain).sorted(Comparator.comparingLong(Instrumento::getId)).toList());
         domain.setAgenda(entity.getAgenda());
         return domain;
     }
@@ -23,9 +24,10 @@ public class LevitaMapper {
         LevitaEntity entity = new LevitaEntity();
         entity.setId(domain.getId());
         entity.setNome(domain.getNome());
-        entity.setInstrumentos(domain.getInstrumentos().stream().map(InstrumentoMapper::domainToEntity).toList());
-        entity.setContato(domain.getContato());
         entity.setEmail(domain.getEmail());
+        entity.setContato(domain.getContato());
+        entity.setDescricao(domain.getDescricao());
+        entity.setInstrumentos(domain.getInstrumentos().stream().map(InstrumentoMapper::domainToEntity).toList());
         entity.setAgenda(domain.getAgenda());
         return entity;
     }

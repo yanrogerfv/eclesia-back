65d07c5743fd9e7cad4e9c04a6f0dfe09483af17
yan-roger-fv <yanrogerfv@gmail.com>
2024-12-20 15:43:07 -0300

    feat: start development of getting users from database

---
+++ src/main/java/imdl/scalator/auth/configuration/AppUserDetailsService.java
@@ -0,0 +1,23 @@
+package imdl.scalator.auth.configuration;
+
+import imdl.scalator.auth.dto.UserDTO;
+import imdl.scalator.auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return UserDTO.toDTO(userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found")));
+    }
+}
